e9effd5485fce17c714d6a4a8cc811e6c2a205a9
Mike Churvis <mikechurvis@gmail.com>
2023-01-05 06:06:53 -0500

    Django: rename app auth -> todoauth

--- todo_api_django/todo_api_django/settings.py
+++ todo_api_django/todo_api_django/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "auth",
+    "todoauth",
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "auth.User"
+AUTH_USER_MODEL = "todoauth.User"
